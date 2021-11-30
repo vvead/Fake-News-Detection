305b7fb74404b839516504c9c1649838a818f0f8
vvead <w.touhami@esi-sba.dz>
2021-11-30 15:51:10 +0100

    training the model

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, escape, request, render_template
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+
+# Load model and tokenizer
+tokenizer= pickle.load(open('tokenizer2.pkl', 'rb')) 
+model = pickle.load(open('model2.pkl', 'rb'))
+#w2v_model = pickle.load(open('w2v_model2.pkl', 'rb'))
+max_length = 1000
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+    
+@app.route("/prediction", methods=['GET','POST'])
+def prediction():
+    if request.method == "POST":
+        news = [request.form['news']]
+
+        
+        print(news)
+        news = tokenizer.texts_to_sequences(news)
+        print(news)
+        news = pad_sequences(news, maxlen=max_length)
+        
+        val_pkl = news
+        predict ='FAKE' if ((model.predict(val_pkl)>=0.5).astype(int)).all() == 0 else 'REAL'
+
+        print(predict)
+        return render_template("prediction.html", prediction_text="News headline is -> {}".format(predict))
+        
+    else: 
+        return render_template("prediction.html")
+    
+
+if __name__ == "__main__":
+    app.run()
